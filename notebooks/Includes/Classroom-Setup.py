41bd7e8341c87056b70ea23582f9b6c687a56746
willblock <williamfblock@gmail.com>
2021-12-02 14:57:49 -0800

    updating classroom-setup

--- notebooks/Includes/Classroom-Setup.py
+++ notebooks/Includes/Classroom-Setup.py
@@ -1,12 +1,9 @@
 # Databricks notebook source
-# MAGIC 
-# MAGIC %scala
-# MAGIC val tags = com.databricks.logging.AttributionContext.current.tags
-# MAGIC val name = tags.getOrElse(com.databricks.logging.BaseTagDefinitions.TAG_USER, java.util.UUID.randomUUID.toString.replace("-", ""))
-# MAGIC val username = if (name != "unknown") name else dbutils.widgets.get("databricksUsername")
-# MAGIC spark.conf.set("com.databricks.training.username", username)
-# MAGIC 
-# MAGIC displayHTML("Initialized classroom variables & functions...")
+username = spark.sql("SELECT current_user()").collect()[0][0]
+
+spark.conf.set("com.databricks.training.username", username)
+
+displayHTML("Initialized classroom variables & functions...")
 
 # COMMAND ----------
 
@@ -20,6 +17,8 @@ dbutils.fs.mkdirs("dbfs:/user/" + username + "/" + course_name)
 
 database_name = username + "_" + course_name
 database_name = database_name.replace(".", "_").replace("@", "_")
+
+
 try:
     spark.sql(f"CREATE DATABASE {database_name}")
 except:
@@ -29,7 +28,9 @@ base_read_path = "wasbs://courseware@dbacademy.blob.core.windows.net/introductio
 london_read_path = base_read_path + "london/"
 base_write_path = "dbfs:/user/" + username + "/" + course_name + "/"
 
+
 input_path = london_read_path + "london-listings-2021-01-31"
+
 
 None
 
